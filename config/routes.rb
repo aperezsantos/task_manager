8401a8e476054c5010d80245d055d4b30dd82abe
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-02 02:30:20 -0600

    Create welcome index route, controller and index action, index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #anytime you get a request (get, post, etc.) for the '/uri', go to: the 'controller and perform the #action'
+  get '/', to: 'welcome#index'
 end
